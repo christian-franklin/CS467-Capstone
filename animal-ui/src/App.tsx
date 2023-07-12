88651bd3686a18a85fcdc0d63a27f65c29e65f70
Christian Franklin <christian.m.franklin@gmail.com>
2023-07-12 18:01:44 -0400

    feat: Auth0 implementation

--- animal-ui/src/App.tsx
+++ animal-ui/src/App.tsx
@@ -1,6 +1,13 @@
 import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
+import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
 
 function App() {
+
+  const { error } = useAuth0();
+  if (error) {
+    return <div>Oops... {error.message}</div>;
+  }
+
   return (
     <Grid
       templateAreas={{
@@ -23,4 +30,7 @@ function App() {
   );
 }
 
-export default App;
+//export default App;
+export default withAuthenticationRequired(App, {
+  onRedirecting: () => <div>Redirecting you to the login page...</div>
+})
