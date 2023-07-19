5128cf042fe6ff895adf55aeff1afde8f15473fb
Christian Franklin <christian.m.franklin@gmail.com>
2023-07-18 21:43:05 -0400

    Feat: Google Cloud setup + Docker

--- animal-api/server.js
+++ animal-api/server.js
@@ -1,17 +1,60 @@
 const express = require("express");
 const { Datastore } = require('@google-cloud/datastore');
+const bodyParser = require("body-parser");
+const request = require("request");
+const jwt = require("express-jwt");
+const jwksRsa = require("jwks-rsa");
+const { auth, requiresAuth } = require("express-openid-connect");
+const path = require("path");
 
-const datastore = new Datastore();
+const datastore = new Datastore({
+  projectId: process.env.projectId,
+});
+
+const ANIMAL = "Animal"
 
 const app = express();
 
 const router = express.Router();
 const login = express.Router();
 
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.secret,
+  baseURL: process.env.baseURL,
+  clientID: process.env.clientID,
+  issuerBaseURL: process.env.issuerBaseURL,
+};
+
+const CLIENT_ID = config.clientID;
+const CLIENT_SECRET = config.secret;
+const DOMAIN = process.env.DOMAIN;
+
+require("dotenv").config();
 app.use(express.json());
 
 app.use("/", router);
 app.use("/login", login);
+
+/* ------------- Helper Functions -------------------------*/
+function fromDatastore(item) {
+  item.id = item[Datastore.KEY].id;
+  return item;
+}
+
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${DOMAIN}/.well-known/jwks.json`,
+  }),
+
+  // Validate the audience and the issuer.
+  issuer: `https://${DOMAIN}/`,
+  algorithms: ["RS256"],
+});
 
 // Custom error handling to send error as JSON instead of HTML
 app.use(function (err, req, res, next) {
@@ -27,20 +70,26 @@ app.use(function (err, req, res, next) {
 // ****************************************
 // CRUD functionality for animal profiles *
 // ****************************************
-app.get('/api/animals', async (req, res) => {
+
+app.get('/', async (req, res) => {
+  console.log('Application entrance');
+  res.json("success");
+})
+
+app.get('/animals', async (req, res) => {
   const query = datastore.createQuery('Animal');
   const [animals] = await datastore.runQuery(query);
   res.json(animals);
 });
 
-app.get('/api/animals/:id', async (req, res) => {
+app.get('/animals/:id', async (req, res) => {
   const animalKey = datastore.key(['Animal', parseInt(req.params.id, 10)]);
   const [animal] = await datastore.get(animalKey);
   if (!animal) return res.status(404).send('The animal with the given ID was not found.');
   res.json(animal);
 });
 
-app.post('/api/animals', async (req, res) => {
+app.post('/animals', async (req, res) => {
   const { key, ...animalData } = req.body;
   const animalKey = datastore.key(['Animal']);
   const entity = {
@@ -51,7 +100,7 @@ app.post('/api/animals', async (req, res) => {
   res.json({ id: animalKey.id, ...animalData });
 });
 
-app.put('/api/animals/:id', async (req, res) => {
+app.put('/animals/:id', async (req, res) => {
   const animalKey = datastore.key(['Animal', parseInt(req.params.id, 10)]);
   const [animal] = await datastore.get(animalKey);
   if (!animal) return res.status(404).send('The animal with the given ID was not found.');
@@ -65,7 +114,7 @@ app.put('/api/animals/:id', async (req, res) => {
   res.json({ id: animalKey.id, ...animal, ...updatedData });
 });
 
-app.delete('/api/animals/:id', async (req, res) => {
+app.delete('/animals/:id', async (req, res) => {
   const animalKey = datastore.key(['Animal', parseInt(req.params.id, 10)]);
   await datastore.delete(animalKey);
   res.status(204).send();
@@ -74,6 +123,8 @@ app.delete('/api/animals/:id', async (req, res) => {
 // END CRUD functionality for animal profiles *
 // ********************************************
 
+app.use("/", router);
+app.use("/login", login);
 
 // Listen to the App Engine-specified port, or 8080 otherwise
 const PORT = process.env.PORT || 8080;
