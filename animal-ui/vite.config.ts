3fa8da703491c267f75c12f180b198335c7e4f9c
Christian Franklin <christian.m.franklin@gmail.com>
2023-08-01 19:37:44 -0400

    Feat: Google Compute Engine sucks

--- animal-ui/vite.config.ts
+++ animal-ui/vite.config.ts
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  preview: {
+    host: true,
+    port: 4173,
+  },
+  server: {
+    host: "0.0.0.0",
+    port: 4173,
+    https: true,
+  },
   plugins: [react()],
 })
