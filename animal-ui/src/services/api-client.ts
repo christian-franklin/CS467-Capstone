d9742713bca9fb50a3edd594fb75a1aeeec088dc
Christian Franklin <christian.m.franklin@gmail.com>
2023-08-05 17:02:19 -0400

    Fix: uncommented https and baseurl

--- animal-ui/src/services/api-client.ts
+++ animal-ui/src/services/api-client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  //baseURL: "https://cs467-capstone-393117.ue.r.appspot.com",
-  baseURL: "http://localhost:8080",
+  baseURL: "https://cs467-capstone-393117.ue.r.appspot.com",
+  //baseURL: "http://localhost:8080",
 });
--- animal-ui/vite.config.ts
+++ animal-ui/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: 4173,
-    //https: true,
+    https: true,
   },
   plugins: [react()],
 });
